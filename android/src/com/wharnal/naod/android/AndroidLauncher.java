e24e5463af199e6f3e31ad959b5d1c9b9fb908f9
Jack Lynch <jacklynch@wharnal.com>
2015-01-10 17:30:00 -0500

    created libGDX projects with gradle

---
+++ android/src/com/wharnal/naod/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wharnal.naod.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wharnal.naod.NaodGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NaodGame(), config);
+	}
+}
---
+++ desktop/src/com/wharnal/naod/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wharnal.naod.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wharnal.naod.NaodGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NaodGame(), config);
+	}
+}
